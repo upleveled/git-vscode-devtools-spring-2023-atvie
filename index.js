1deb849b61bb15ea52df5b4c53cc5ea30abe8621
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-05-05 12:18:13 +0200

    Add animal array

---
+++ index.js
@@ -0,0 +1,8 @@
+const animals = [
+  { id: 1, name: 'Maurizio', type: 'Goat', age: 12 },
+  { id: 2, name: 'Lucia', type: 'Cat', age: 12 },
+  { id: 3, name: 'Markus', type: 'Rabbit', age: 12 },
+  { id: 4, name: 'Marta', type: 'Dog', age: 12 },
+  { id: 4, name: 'Marta', type: 'Dog', age: 12 },
+  { id: 5, name: 'Francy', type: 'Dog', age: 12 },
+];
